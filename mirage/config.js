65a87581f3634ed7facb9d1bde175442363a8ed8
Todd Jordan <todd.jordan@inin.com>
2016-02-10 23:02:49 -0500

    Fix build, data format

--- mirage/config.js
+++ mirage/config.js
@@ -10,31 +10,43 @@ export default function() {
 
   this.get('/rentals', function() {
     return {
-      rentals: [{
-        "id": 1,
-        "owner": "Veruca Salt",
-        "city": "San Francisco",
-        "type": "Estate",
-        "bedrooms": 15,
-        "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
-      }, {
-        "id": 2,
-        "owner": "Mike Teavee",
-        "city": "Seattle",
-        "type": "Condo",
-        "bedrooms": 1,
-        "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
-      }, {
-        "id": 3,
-        "owner": "Violet Beauregarde",
-        "city": "Portland",
-        "type": "Apartment",
-        "bedrooms": 3,
-        "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
-      }]
+      data: [
+        {
+          type: 'rental',
+          id: 1,
+          attributes: {
+            "owner": "Veruca Salt",
+            "city": "San Francisco",
+            "type": "Estate",
+            "bedrooms": 15,
+            "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+          }
+        },
+        {
+          type: 'rental',
+          "id": 2,
+          attributes: {
+            "owner": "Mike Teavee",
+            "city": "Seattle",
+            "type": "Condo",
+            "bedrooms": 1,
+            "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+          }
+        },
+        {
+          type: 'rental',
+          "id": 3,
+          attributes: {
+            "owner": "Violet Beauregarde",
+            "city": "Portland",
+            "type": "Apartment",
+            "bedrooms": 3,
+            "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+          }
+        }
+      ]
     }
-  })
-
+  });
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
