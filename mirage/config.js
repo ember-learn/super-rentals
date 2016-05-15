153f444a81e49930c9fbae14cc12b5a6053f432d
Scott Batson <sjbats@email.com>
2016-05-15 12:01:22 -0400

    add get/id route to ember-cli-mirage

--- mirage/config.js
+++ mirage/config.js
@@ -8,6 +8,54 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
+  this.get('/rentals/:id', function(db, request) {
+    console.log(request);
+    let rentals = [
+      {
+        type: 'rentals',
+        id: 1,
+        attributes: {
+          "title": "Grand Old Mansion",
+          "owner": "Veruca Salt",
+          "city": "San Francisco",
+          "type": "Estate",
+          "bedrooms": 15,
+          "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+        }
+      },
+      {
+        type: 'rentals',
+        "id": 2,
+        attributes: {
+          "title": "Urban Living",
+          "owner": "Mike Teavee",
+          "city": "Seattle",
+          "type": "Condo",
+          "bedrooms": 1,
+          "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+        }
+      },
+      {
+        type: 'rentals',
+        "id": 3,
+        attributes: {
+          "title": "Downtown Charm",
+          "owner": "Violet Beauregarde",
+          "city": "Portland",
+          "type": "Apartment",
+          "bedrooms": 3,
+          "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+        }
+      }
+    ];
+
+    const selectedRental = rentals.find(function(rental) {
+     return rental.id.toString() === request.params.id;
+    });
+
+    return { data: selectedRental };
+  });
+
   this.get('/rentals', function(db, request) {
     let rentals = [
       {
