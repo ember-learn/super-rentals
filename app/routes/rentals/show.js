f31fd873d189fcba679027b68354486b4798dfdb
Scott Batson <sjbats@email.com>
2016-07-15 22:02:43 -0400

    redirect to rentals route from index

--- app/routes/rentals/show.js
+++ app/routes/rentals/show.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.queryRecord('rental', { slug: params.slug });
+    return this.get('store').queryRecord('rental', { slug: params.slug });
   }
 });
