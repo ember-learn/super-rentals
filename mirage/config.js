02a4dc3c62bf46f733b6974b68280be1fe257024
Scott Batson <sjbats@email.com>
2016-08-08 20:39:34 -0400

    add api/ namespace for mirage requests

--- mirage/config.js
+++ mirage/config.js
@@ -8,6 +8,8 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
+  this.namespace = '/api';
+
   this.get('/rentals', function(db, request) {
     let rentals = [
       {
@@ -61,7 +63,6 @@ export default function() {
 
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
