28f2f93c0ec2209487c68cf3ff8dfdcdd6250b30
Todd Jordan <todd.jordan@inin.com>
2016-09-09 13:47:03 -0400

    update test

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -5,7 +5,7 @@ moduleFor('route:index', 'Unit | Route | index');
 test('should transition to rentals route', function(assert) {
   let route = this.subject({
     replaceWith(routeName) {
-      assert.equal(routeName, 'rentals.index', 'transition to route name rentals.index');
+      assert.equal(routeName, 'rentals', 'transition to route name rentals');
     }
   });
   route.beforeModel();
