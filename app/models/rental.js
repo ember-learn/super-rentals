f2ca7b5966e95fad202f4bb92eaa063602bd8b12
Jen Weber <jen@biobright.org>
2017-05-24 01:10:08 -0400

    change superRentals type to propertyType due to JSONAPI (#68) (#69)

--- app/models/rental.js
+++ app/models/rental.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr(),
   owner: DS.attr(),
   city: DS.attr(),
-  type: DS.attr(),
+  propertyType: DS.attr(),
   image: DS.attr(),
   bedrooms: DS.attr(),
   description: DS.attr()
