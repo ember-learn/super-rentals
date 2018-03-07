5feb9ea13dfff554a3564fb267fb8e5d3d7cb405
Todd Jordan <todd.jordan@gmail.com>
2018-03-07 09:58:57 -0500

    upgrade to 3.0 (#95)

--- tests/unit/routes/rentals-test.js
+++ tests/unit/routes/rentals-test.js
@@ -1,11 +1,11 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('route:rentals', 'Unit | Route | rentals', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+module('Unit | Route | rentals', function(hooks) {
+  setupTest(hooks);
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  test('it exists', function(assert) {
+    let route = this.owner.lookup('route:rentals');
+    assert.ok(route);
+  });
 });
--- tests/unit/routes/rentals/show-test.js
+++ tests/unit/routes/rentals/show-test.js
@@ -1,23 +1,25 @@
 import { run } from '@ember/runloop';
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import startMirage from '../../../helpers/setup-mirage-for-unit-test';
 
-moduleFor('route:rentals/show', 'Unit | Route | rentals/show', {
-  needs: ['model:rental',
-         'adapter:application'],
-  beforeEach() {
-    startMirage(this.container);
-  },
-  afterEach() {
-    window.server.shutdown();
-  }
-});
+module('Unit | Route | rentals/show', function(hooks) {
+  setupTest(hooks);
 
-test('should load rental by id', function(assert) {
-  let route = this.subject();
-  return run(() => {
-    return route.model({ rental_id: 'grand-old-mansion'}).then((result) => {
-      assert.equal(result.get('title'), "Grand Old Mansion");
+  hooks.beforeEach(function() {
+    startMirage(this.container);
+  });
+
+  hooks.afterEach(function() {
+    window.server.shutdown();
+  });
+
+  test('should load rental by id', function(assert) {
+    let route = this.owner.lookup('route:rentals/show');
+    return run(() => {
+      return route.model({ rental_id: 'grand-old-mansion'}).then((result) => {
+        assert.equal(result.get('title'), "Grand Old Mansion");
+      });
     });
   });
 });
