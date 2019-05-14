3300dc9d21c2fefc6f8ba92401184804c9551296
Ricardo Mendes <rokusu@gmail.com>
2019-05-14 10:40:18 +0100

    fix index route test

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,13 +1,13 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Route | index', function(hooks) {
+module("Unit | Route | index", function(hooks) {
   setupTest(hooks);
 
-  test('should transition to rentals route', function(assert) {
-    let route = this.owner.factoryFor('route:index').create({
-      replaceWith(routeName) {
-        assert.equal(routeName, 'rentals', 'transition to route name rentals');
+  test("should transition to rentals route", function(assert) {
+    let route = this.owner.factoryFor("route:index").create({
+      transitionTo(routeName) {
+        assert.equal(routeName, "rentals", "transition to route name rentals");
       }
     });
     route.beforeModel();
