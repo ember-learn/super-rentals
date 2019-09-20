ca7e297f1f58f09552fac5475811ce47f45393b2
Tomster <tomster@emberjs.com>
2019-09-20 17:16:32 +0000

    Merge ember-learn/super-rentals-tutorial#56
    Fix CI
    
    ---
    
    Commit:  ember-learn/super-rentals-tutorial@0fbc7ad878e2ac8d931d5f6515ef2ef6e51b31df
    Script:  https://github.com/ember-learn/super-rentals-tutorial/blob/0fbc7ad878e2ac8d931d5f6515ef2ef6e51b31df/scripts/deploy.sh
    Logs:    https://travis-ci.org/ember-learn/super-rentals-tutorial/jobs/587568894

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import ENV from 'super-rentals/config/environment';
 
 module('Integration | Component | map', function(hooks) {
