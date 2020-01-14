f7637f0c63ecc4580a2fba1b67d401a19c3175fb
Tomster <tomster@emberjs.com>
2020-01-14 00:20:32 +0000

    Chapter 6

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function(hooks) {
@@ -17,5 +17,29 @@ module('Integration | Component | rental/image', function(hooks) {
     assert.dom('.image').exists();
     assert.dom('.image img').hasAttribute('src', '/assets/images/teaching-tomster.png');
     assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('clicking on the component toggles its size', async function(assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large')
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
