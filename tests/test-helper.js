590c5ee2dc1d232c2ecb643dc0a0fd7cb302dce2
Tomster <tomster@emberjs.com>
2020-07-28 00:32:45 +0000

    [CRON] Tuesday Jul 28, 2020
    ---
    
    Commit:  ember-learn/super-rentals-tutorial@d211925aed4d9287fb149886db86dee266b29937
    Script:  https://github.com/ember-learn/super-rentals-tutorial/blob/d211925aed4d9287fb149886db86dee266b29937/.github/workflows/build.yml
    Logs:    https://github.com/ember-learn/super-rentals-tutorial/commit/d211925aed4d9287fb149886db86dee266b29937/checks

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'super-rentals/app';
+import config from 'super-rentals/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
