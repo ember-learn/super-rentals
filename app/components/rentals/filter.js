b05b9b661de351fea7f434005146932b95150165
Tomster <tomster@emberjs.com>
2021-03-13 01:13:06 +0000

    Chapter 12

---
+++ app/components/rentals/filter.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+
+export default class RentalsFilterComponent extends Component {
+  get results() {
+    let { rentals, query } = this.args;
+
+    if (query) {
+      rentals = rentals.filter((rental) => rental.title.includes(query));
+    }
+
+    return rentals;
+  }
+}
