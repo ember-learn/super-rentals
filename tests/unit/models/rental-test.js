a92f8273c44d6408ae00e4e28e521390bd039a4b
Tomster <tomster@emberjs.com>
2022-04-08 02:08:52 +0000

    [CRON] Friday Apr 08, 2022
    ---
    
    Commit:  ember-learn/super-rentals-tutorial@723f119fe77897630639c14d0fdd79a089a9043b
    Script:  https://github.com/ember-learn/super-rentals-tutorial/blob/723f119fe77897630639c14d0fdd79a089a9043b/.github/workflows/build.yml
    Logs:    https://github.com/ember-learn/super-rentals-tutorial/commit/723f119fe77897630639c14d0fdd79a089a9043b/checks

--- tests/unit/models/rental-test.js
+++ tests/unit/models/rental-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupTest } from 'super-rentals/tests/helpers';
 
 module('Unit | Model | rental', function (hooks) {
   setupTest(hooks);
