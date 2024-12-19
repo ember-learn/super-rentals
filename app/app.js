c73b743d12295b2b83cdc4fa0cdf1efe883a969c
Tomster <tomster@emberjs.com>
2024-12-19 00:46:59 +0000

    Chapter 11

--- app/app.js
+++ app/app.js
@@ -2,6 +2,11 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'super-rentals/config/environment';
+import { setBuildURLConfig } from '@ember-data/request-utils';
+
+setBuildURLConfig({
+  namespace: 'api',
+});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
