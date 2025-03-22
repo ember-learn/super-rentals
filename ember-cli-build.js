8b0fb44776e6da0c2f20c0611433717e95a53c9e
Peter C. S. Scholtens <peter.scholtens@freedom.nl>
2025-03-22 16:07:35 +0100

    Deprecate Store class extention
    Deprecates the Store class extending from EmberObject.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    emberData: {
+      deprecations: {
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false
+      }
+    }
   });
 
   return app.toTree();
