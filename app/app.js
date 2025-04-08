344f733ebc95fa5dc52b25b7d3142b7f3a53a85b
Tomster <tomster@emberjs.com>
2025-04-08 15:29:07 +0000

    Upgrade puppeteer
    ---
    
    Commit:  ember-learn/super-rentals-tutorial@43f1a8e6605a13775ebce3422e6ca3b5109170eb
    Script:  https://github.com/ember-learn/super-rentals-tutorial/blob/43f1a8e6605a13775ebce3422e6ca3b5109170eb/.github/workflows/build.yml
    Logs:    https://github.com/ember-learn/super-rentals-tutorial/commit/43f1a8e6605a13775ebce3422e6ca3b5109170eb/checks

--- app/app.js
+++ app/app.js
@@ -2,11 +2,16 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'super-rentals/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import { setBuildURLConfig } from '@ember-data/request-utils';
 
 setBuildURLConfig({
   namespace: 'api',
 });
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
