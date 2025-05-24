0f032143f845e17506712bc3afbdac3314502f06
Tomster <tomster@emberjs.com>
2025-05-24 00:55:23 +0000

    Chapter 11

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,12 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
+import { query } from '@ember-data/json-api/request';
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
-
-    return data.map((model) => {
-      let { id, attributes } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
+    const { content } = await this.store.request(query('rental'));
+    return content.data;
   }
 }
