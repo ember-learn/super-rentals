a4924847d01310048c62f48eb9d38ae1706754b2
Tomster <tomster@emberjs.com>
2025-06-08 01:02:13 +0000

    Chapter 9

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -9,6 +9,7 @@ module('Integration | Component | rental', function (hooks) {
   test('it renders information about a rental property', async function (assert) {
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -30,6 +31,9 @@ module('Integration | Component | rental', function (hooks) {
 
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert
+      .dom('article h3 a')
+      .hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
