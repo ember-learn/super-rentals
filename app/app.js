cf63eb16c3afbeb0a553bd991135106a2b90edc0
Tomster <tomster@emberjs.com>
2025-09-10 01:01:20 +0000

    Chapter 11

--- app/app.js
+++ app/app.js
@@ -3,6 +3,24 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'super-rentals/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import { setBuildURLConfig } from '@ember-data/request-utils';
+
+setBuildURLConfig({
+  namespace: 'api',
+});
+
+/* This is to account for a deprecation that shipped in ember-cli 6.4
+   with ember-data v5.6 which needs a blueprint update to avoid the
+   deprecation that is otherwise irrelevant for tutorial purposes.
+*/
+import { registerDeprecationHandler } from '@ember/debug';
+registerDeprecationHandler((message, options, next) => {
+  if (message.includes('Using WarpDrive with EmberJS requires')) {
+    return;
+  } else {
+    next(message, options);
+  }
+});
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
