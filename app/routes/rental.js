c4230b72da15bc1bd479c2e372d24daf53104148
Tomster <tomster@emberjs.com>
2025-10-31 01:13:34 +0000

    Chapter 11

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,21 +1,14 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
+import { findRecord } from '@ember-data/json-api/request';
 
 export default class RentalRoute extends Route {
+  @service store;
+
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-
-    let { id, attributes } = data;
-    let type;
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'Community';
-    } else {
-      type = 'Standalone';
-    }
-
-    return { id, type, ...attributes };
+    const { content } = await this.store.request(
+      findRecord('rental', params.rental_id),
+    );
+    return content.data;
   }
 }
