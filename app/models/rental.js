24d64d610f75511679305edba620a4d8c08fc8ef
Tomster <tomster@emberjs.com>
2026-01-31 02:57:33 +0000

    Chapter 11

---
+++ app/models/rental.js
@@ -0,0 +1,22 @@
+import Model, { attr } from '@warp-drive/legacy/model';
+
+const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+
+export default class RentalModel extends Model {
+  @attr title;
+  @attr owner;
+  @attr city;
+  @attr location;
+  @attr category;
+  @attr image;
+  @attr bedrooms;
+  @attr description;
+
+  get type() {
+    if (COMMUNITY_CATEGORIES.includes(this.category)) {
+      return 'Community';
+    } else {
+      return 'Standalone';
+    }
+  }
+}
