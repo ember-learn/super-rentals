a3bb3e455e143176dcd333bc1cc202f957f52133
Tomster <tomster@emberjs.com>
2026-08-20 01:56:50 +0000

    Chapter 9

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ export default class IndexRoute extends Route {
     let { data } = await response.json();
 
     return data.map((model) => {
-      let { attributes } = model;
+      let { id, attributes } = model;
       let type;
 
       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
@@ -17,7 +17,7 @@ export default class IndexRoute extends Route {
         type = 'Standalone';
       }
 
-      return { type, ...attributes };
+      return { id, type, ...attributes };
     });
   }
 }
---
+++ app/templates/rental.gjs
@@ -0,0 +1,5 @@
+import RentalDetailed from 'super-rentals/components/rental/detailed';
+
+<template>
+  <RentalDetailed @rental={{@model}} />
+</template>
